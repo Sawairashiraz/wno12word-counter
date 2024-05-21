c98f889ca772c98b15911585485119383a77cb93
Sawairashiraz <165516821+Sawairashiraz@users.noreply.github.com>
2024-05-22 01:33:33 +0500

    Add files via upload

---
+++ wc.ts
@@ -0,0 +1,37 @@
+#!/user/bin/env node
+import inquirer from "inquirer";
+
+const answers =await
+inquirer.prompt({
+    name: "sentence",
+    type: "input",
+    message: "Enter your sentence to count the word?",
+
+});
+const words =
+answers.sentence.trim().split(``);
+console.log(words);
+console.log(`your sentence word count is ${words.length}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
